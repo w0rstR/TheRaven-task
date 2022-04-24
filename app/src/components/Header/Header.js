97e8c466849d6702058d465ebd491229490ec3aa
w0rstR <sat2151@gmail.com>
2022-04-25 00:01:37 +0300

    added header and items

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,13 +1,33 @@
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 import s from "./Header.module.css"
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        right: -3,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}));
+
 export default function Header(){
     return(
         <div className={s.container}>
-            <div>
-                Header
-            </div>
-            <button className={s.btn}>
-                Card
-            </button>
+                <div>
+                    <button className={s.btn}>
+                        Card
+                    </button>
+                </div>
+                <div>
+                    <IconButton aria-label="cart">
+                        <StyledBadge badgeContent={4} color="secondary">
+                            <ShoppingCartIcon />
+                        </StyledBadge>
+                    </IconButton>
+                </div>
         </div>
     )
 }
--- app/src/components/ProductItem/ProductItem.js
+++ app/src/components/ProductItem/ProductItem.js
@@ -3,10 +3,12 @@ import s from "./ProductItem.module.css"
 export default function ProductItem(){
     return(
         <div className={s.container}>
-            <div>img</div>
-            <div>Title</div>
-            <div>Description</div>
-            <div>add to bag</div>
+            <div className={s.itemBody}>
+                <img className={s.img} src="https://images.unsplash.com/photo-1536304929831-ee1ca9d44906?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ" alt=""/>
+                <h2 className={s.title}>title</h2>
+                <p className={s.decription}>Desc</p>
+            </div>
+            <button className={s.btn}>Add</button>
         </div>
     )
 }
